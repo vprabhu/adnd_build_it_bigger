d202552d4c48773c86415f4601c7213dfc1a0e9f
vignesh prabhu <vignesh.p@ninestars.in>
2018-03-20 12:45:09 +0530

    feat : add joke java lib

---
+++ jokelib/src/main/java/vhp/com/jokelib/JokeProducer.java
@@ -0,0 +1,12 @@
+package vhp.com.jokelib;
+
+public class JokeProducer {
+
+    /**
+     * This method return the Moses tablet joke
+     * @return moses joke
+     */
+    public String getMosesJoke(){
+        return "Moses had the first tablet that could connect to the cloud.";
+    }
+}
