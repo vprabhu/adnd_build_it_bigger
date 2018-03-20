f696a5625b87a311217549f5ae7e05e3d0e17261
vignesh prabhu <vignesh.p@ninestars.in>
2018-03-20 13:04:41 +0530

    feat : wire up java lib and android lib .. to display the joke

--- app/src/main/java/vhp/com/jokeassemble/MainActivity.kt
+++ app/src/main/java/vhp/com/jokeassemble/MainActivity.kt
@@ -1,5 +1,6 @@
 package vhp.com.jokeassemble
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -7,6 +8,7 @@ import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdView
 import com.google.android.gms.ads.MobileAds
 import kotlinx.android.synthetic.main.activity_main.*
+import vhp.com.displayjoke.DisplayJokeActivity
 import vhp.com.jokelib.JokeProducer
 
 class MainActivity : AppCompatActivity() {
@@ -27,7 +29,14 @@ class MainActivity : AppCompatActivity() {
 
         // display joke button button listener
         button_display_joke.setOnClickListener(){
+            //initialize the JokeProducer class from java library
             val mjokeProducer = JokeProducer()
+            // launch the displayjoke library activity to display the joke and
+            // send the joke as extras
+            val displayJokeIntent = Intent(this , DisplayJokeActivity::class.java)
+            displayJokeIntent.putExtra("joke" , mjokeProducer.mosesJoke)
+            startActivity(displayJokeIntent)
+            // show the toast to display the  joke from JavaLibrary
             Toast.makeText(this , mjokeProducer.mosesJoke , Toast.LENGTH_LONG).show()
         }
 
