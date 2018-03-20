80bf92ccbce9cbaeacfb9792734872239e3d3a1c
vignesh prabhu <vignesh.p@ninestars.in>
2018-03-20 12:46:16 +0530

    feat : add joke java library to android app

--- app/src/main/java/vhp/com/jokeassemble/MainActivity.kt
+++ app/src/main/java/vhp/com/jokeassemble/MainActivity.kt
@@ -2,9 +2,12 @@ package vhp.com.jokeassemble
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdView
 import com.google.android.gms.ads.MobileAds
+import kotlinx.android.synthetic.main.activity_main.*
+import vhp.com.jokelib.JokeProducer
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,9 +21,15 @@ class MainActivity : AppCompatActivity() {
         MobileAds.initialize(this, resources.getString(R.string.key_admob))
 
         // setting up the AdView to show the ad
-        mAdView = findViewById(R.id.adView)
+        mAdView = this.findViewById(R.id.adView)
         val adRequest = AdRequest.Builder().build()
         mAdView.loadAd(adRequest)
+
+        // display joke button button listener
+        button_display_joke.setOnClickListener(){
+            val mjokeProducer = JokeProducer()
+            Toast.makeText(this , mjokeProducer.mosesJoke , Toast.LENGTH_LONG).show()
+        }
 
     }
 }
